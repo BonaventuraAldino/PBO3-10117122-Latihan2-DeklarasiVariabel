28032e9e7a86db47e03a321aed78b5f504e3a5d2
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-22 23:24:12 +0700

    PBO3-10117122-Latihan2-DeklarasiVariabel

---
+++ src/pbo3/pkg10117122/latihan2/deklarasivariabel/PBO310117122Latihan2DeklarasiVariabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * 
+ */
+public class PBO310117122Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi varibel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+}
